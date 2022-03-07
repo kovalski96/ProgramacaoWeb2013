ec9740741f5fa43a8be8dbe84ff986c97f3e60ff
Bruno Kovalski <brunokovalski15@gmail.com>
2022-03-06 21:35:31 -0300

    first commit

---
+++ web/atividade 25 php.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+echo "<table style='width:100%;'>";
+
+
+for($x=1;$x<254;$x++){
+
+echo "<tr><td style='background-color:rgb(".$x.",".$x.",".$x.");'>".$x."</td></tr>";
+}
+
+echo "</table>";
+
+?>
+
+
+
+
+
+
---
+++ web/atividade 26 php.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+echo "<table style='width:100%;' border=1>";
+
+
+for($x=1;$x<2000;$x++){
+
+echo "<tr><td>".$x."</td><td>".$x."</td><td>".$x."</td></tr>";
+
+
+}
+
+echo "</table>";
+
+?>
+
+
+
+
+
+
